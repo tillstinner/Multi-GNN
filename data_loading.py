6b4ffc9e6b3788667006532f172da06af4516554
tstinner <tillstinner@posteo.de>
2026-01-17 11:12:22 +0100

    init thesis/hpc

--- data_loading.py
+++ data_loading.py
@@ -13,7 +13,7 @@ def get_data(args, data_config):
     3. PyG Data objects are created with the respective data splits.
     '''
 
-    transaction_file = f"{data_config['paths']['aml_data']}/{args.data}/formatted_transactions.csv" #replace this with your path to the respective AML data objects
+    transaction_file = f"{data_config['paths']['aml_data']}/{args.data}" #replace this with your path to the respective AML data objects
     df_edges = pd.read_csv(transaction_file)
 
     logging.info(f'Available Edge Features: {df_edges.columns.tolist()}')
